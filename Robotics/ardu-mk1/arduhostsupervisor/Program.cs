00e3f8be2f361152c44e8107a3fed4a01e68f994
Avdv <3533370+alexander-vd-vegt@users.noreply.github.com>
2024-10-05 23:26:27 +0000

    fkldjfklajf

--- Robotics/ardu-mk1/arduhostsupervisor/Program.cs
+++ Robotics/ardu-mk1/arduhostsupervisor/Program.cs
@@ -1,26 +1,49 @@
 using arduhostsupervisor;
+using arduhostsupervisor.Models;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.AddArduHostSupervisior();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
 
-// app.UseHttpsRedirection();
-
-app.MapGet("/", () =>
+app.MapGet("/", async ([FromServices] DockerClient _dockerClient) =>
 {
-    return "Hello (-:";
+    var listParam = new ContainersListParameters(); 
+    var runningCon =  await _dockerClient.Containers.ListContainersAsync(listParam);
+    var info = runningCon
+        .Where(container => container.Labels.ContainsKey("Ardu"))
+        .Select(con => new ArduComponentContainer{
+            Name = con.Names.FirstOrDefault(),
+            Image = con.Image
+        });
+    return Results.Ok(info.AsEnumerable());
+});
+
+app.MapPost("/", async ([FromBody] ArduComponentContainer container, 
+[FromServices] DockerClient _dockerClient) =>
+{
+    try
+    {
+        var param = new CreateContainerParameters()
+        {
+            Image = container.Image,
+            Name = container.Name,
+            Labels = new Dictionary<string, string(){
+                new KeyValuePair<string, string>(key: "Ardu", value: "")
+            }
+        };
+        await _dockerClient.Containers.CreateContainerAsync(param);
+        return Results.Ok();
+
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest();
+    }
 });
 
 app.Run();
